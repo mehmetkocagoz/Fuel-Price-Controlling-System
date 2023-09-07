cc38de83bc857a2831d1fd130e61a1cedd081725
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-09-07 19:57:35 +0300

    linear regression metrics

--- datafunctions/linearRegression.py
+++ datafunctions/linearRegression.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import numpy as np
 
 dataFrame = pd.read_csv("price_data.csv")
 print(dataFrame.corr(method="pearson",numeric_only=True))
@@ -15,3 +17,9 @@ print(coeff_df)
 predictions = lm.predict(X_test)
 plt.scatter(y_test,predictions)
 plt.show()
+MAE = metrics.mean_absolute_error(y_test,predictions)
+MSE = metrics.mean_squared_error(y_test,predictions)
+RMSE = np.sqrt(metrics.mean_squared_error(y_test,predictions)) 
+print('MAE:',MAE)
+print('MSE:',MSE)
+print('RMSE:',RMSE)
