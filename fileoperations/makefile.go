22c2bee409764146ca920d43424dbc9458fd5cc4
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-10-04 17:33:45 +0300

    JSON convert deleted

--- fileoperations/makefile.go
+++ fileoperations/makefile.go
@@ -1,14 +1,9 @@
 package fileoperations
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"mehmetkocagz/database"
-	"mehmetkocagz/model"
 	"os"
 	"strconv"
-	"time"
 )
 
 func writeHeader(file *os.File) {
@@ -77,44 +72,5 @@ func UpdateCSVFile() {
 			panic(scanError)
 		}
 		file.WriteString(strconv.FormatFloat(price, 'f', -1, 64) + "," + strconv.FormatFloat(fuelPrice, 'f', -1, 64) + "," + strconv.FormatFloat(usdExchangeRateColumn, 'f', -1, 64) + "\n")
-	}
-}
-
-func ConvertJSON() {
-	db := database.Connect()
-	defer db.Close()
-
-	rows, err := db.Query("SELECT * FROM prices order by timestamp")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	var data []model.PriceAnalysis
-	var hold model.PriceAnalysis
-	var timestamp int64
-	for rows.Next() {
-
-		err := rows.Scan(&timestamp, &hold.BrentPrice, &hold.FuelPrice, &hold.FuelPrice)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		hold.PriceAnalysisID = time.Unix(timestamp/1000, 0)
-		fmt.Printf("Timestamp: %s\n", hold.PriceAnalysisID)
-		data = append(data, hold)
-	}
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	outputFile, err := os.Create("output.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outputFile.Close()
-
-	_, err = outputFile.Write(jsonData)
-	if err != nil {
-		log.Fatal(err)
 	}
 }
